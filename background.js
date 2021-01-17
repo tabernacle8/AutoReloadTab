a8f796c57742078d8d53d381bf29d8c61bcc0bba
Carter Kosturos <tabernacle800@gmail.com>
2021-01-16 19:33:47 -0800

    Better

--- background.js
+++ background.js
@@ -1,20 +1,30 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({
+        color: '#3aa757'
+    }, function () {
+        console.log("The color is green.");
     });
-  });
+    chrome.storage.sync.set({
+        reloadSeconds: '10'
+    }, function () {
+        //null
+    });
+});
 
-  chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log('The color is green.');
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({
+        color: '#3aa757'
+    }, function () {
+        console.log('The color is green.');
     });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {urlMatches: '.'},
-        })
-        ],
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {
+                    urlMatches: '.'
+                },
+            })],
             actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
+        }]);
     });
-  });
+});
--- popup.js
+++ popup.js
@@ -1,12 +1,15 @@
-let changeColor = document.getElementById('changeColor');
+let startReload = document.getElementById('startReload');
+let stopReload = document.getElementById('stopReload');
+let userTimeTextbox = document.getElementById('userTimeTextbox');
+console.log("logging....")
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+chrome.storage.sync.get('color', function (data) {
+    //changeColor.style.backgroundColor = data.color;
+    //changeColor.setAttribute('value', data.color);
 });
 
-changeColor.onclick = function(element) {
-    
+//changeColor.onclick = function(element) {
+/*
     let color = element.target.value;
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.executeScript(
@@ -14,7 +17,15 @@ changeColor.onclick = function(element) {
           {code: 'document.body.style.backgroundColor = "' + color + '";'});
     });
   };
+  */
+//}
 
-  function beginReloading(){
-      
-  }
+userTimeTextbox.addEventListener("change", function (data) {
+    console.log(userTimeTextbox.value);
+    userTimeTextbox.Enabled = false;
+})
+
+startReload.onclick = function (element) {
+
+
+}
